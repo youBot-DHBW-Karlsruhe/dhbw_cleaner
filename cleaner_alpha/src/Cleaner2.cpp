52418057269cb9ffe65e745a3b9b7814fec1c82b
CodeLionX <sebastian.schmidl@t-online.de>
2017-02-21 18:10:44 +0100

    worked on torque control and added subscriber for arm positions to manipulator; new test-executable named 'Cleaner2'; TODO: use actual arm positions as start pose and use JS2CS generator

--- cleaner_alpha/src/Cleaner2.cpp
+++ cleaner_alpha/src/Cleaner2.cpp
@@ -4,6 +4,7 @@
 #include "trajectory_generator/CStoCS.h"
 #include "trajectory_generator/JStoCS.h"
 #include "brics_actuator/JointPositions.h"
+#include "sensor_msgs/JointState.h"
 #include "Eigen/Dense"
 
 namespace youbot_proxy {
@@ -52,7 +53,7 @@ public:
     Trajectory(const ros::ServiceClient& generator, const geometry_msgs::Pose start) {
         cs2csGenerator = generator;
         max_acc = DEFAULT_MAX_ACC;
-        max_vel = DEFAULT_MAX_ACC;
+        max_vel = DEFAULT_MAX_VEL;
 
         resetCS2CS(start, 0.0);
     }
@@ -66,25 +67,32 @@ public:
     }
 
     bool addPose(const geometry_msgs::Pose p, double vel) {
+        bool feasible = false;
         cs2cs.request.end_pos = p;
         cs2cs.request.end_vel = checkedVel(vel);
 
         // generate trajectory points and add to trajectory
-        if(cs2csGenerator.call(cs2cs) && cs2cs.response.feasible) {
-            trajectory_msgs::JointTrajectory temp = cs2cs.response.trajectory;
-            t.joint_names = temp.joint_names;
-            trajectory_msgs::JointTrajectoryPoint point;
-            while (!temp.points.empty()) {
-              point = temp.points.back();
-              temp.points.pop_back();
-              t.points.insert(t.points.begin(), point);
-            }
-            // reset cs2cs object to start over at this pose
-            resetCS2CS(p, vel);
+        if(cs2csGenerator.call(cs2cs)) {
+            feasible = cs2cs.response.feasible;
+            if(feasible) {
+                trajectory_msgs::JointTrajectory temp = cs2cs.response.trajectory;
+                trajectory_msgs::JointTrajectoryPoint point;
+                while (!temp.points.empty()) {
+                  point = temp.points.back();
+                  temp.points.pop_back();
+                  t.points.insert(t.points.begin(), point);
+                }
+                t.joint_names = temp.joint_names;
+                // reset cs2cs object to start over at this pose
+                resetCS2CS(p, vel);
 
-            return true;
+                return true;
+            } else {
+                ROS_ERROR("Could not add point to trajectory. Trajectory not feasible!");
+                return false;
+            }
         } else {
-            ROS_ERROR("Could not add point to trajectory.");
+            ROS_ERROR("Could not add point to trajectory. Call to trajectory generator failed!");
             return false;
         }
     }
@@ -103,11 +111,13 @@ class Manipulator {
     private:
 
         ros::Duration timeout;
+        sensor_msgs::JointState jointState;
 
         ros::ServiceClient cs2csGenerator;
         ros::ServiceClient js2csGenerator;
 
         ros::Publisher armPublisher;
+        ros::Subscriber armPositionSubscriber;
 
         actionlib::SimpleActionClient<torque_control::torque_trajectoryAction>* torqueController;
 
@@ -118,12 +128,13 @@ class Manipulator {
         Manipulator(ros::NodeHandle node) {
             timeout = ros::Duration(DEFAULT_TIMEOUT);
 
-            cs2csGenerator = node.serviceClient<trajectory_generator::CStoCS>("CS_to_CS");
-            js2csGenerator = node.serviceClient<trajectory_generator::JStoCS>("JS_to_CS");
+            cs2csGenerator = node.serviceClient<trajectory_generator::CStoCS>("From_CS_to_CS");
+            js2csGenerator = node.serviceClient<trajectory_generator::JStoCS>("From_JS_to_CS");
 
             armPublisher = node.advertise<brics_actuator::JointPositions>("/arm_1/arm_controller/position_command", 1);
+            armPositionSubscriber = node.subscribe<sensor_msgs::JointState>("/joint_states", 1, &Manipulator::armPositionHandler, this);
 
-            torqueController = new actionlib::SimpleActionClient<torque_control::torque_trajectoryAction>(node, "torque_control", true);
+            torqueController = new actionlib::SimpleActionClient<torque_control::torque_trajectoryAction>("torque_control", true);
             ROS_INFO_STREAM("Waiting " << timeout << " seconds for action server to start");
             torqueController->waitForServer(timeout);
             if(!torqueController->isServerConnected()) {
@@ -140,9 +151,22 @@ class Manipulator {
             return Trajectory(cs2csGenerator, start);
         }
 
+        void armPositionHandler(const sensor_msgs::JointStateConstPtr& msg) {
+            jointState = *msg;
+        }
+
+        sensor_msgs::JointState getJointPositions() {
+            ros::spinOnce();
+            ros::spinOnce();
+            return jointState;
+        }
+
         void testTrajectory() {
+
+            // TODO: use actual arm position as start position
+            // --> needs JS2CS
             geometry_msgs::Pose start;
-            start.position.x = 0.25;
+            start.position.x = 0.27;
             start.position.y = 0.00;
             start.position.z = 0.05;
             Eigen::Quaterniond grip(0.6851, 0.1749, 0.6851, -0.1749);
@@ -154,11 +178,11 @@ class Manipulator {
             Trajectory trajectory = newCS2CSTrajectory(start);
 
             geometry_msgs::Pose kp;
-            kp.position.x = 0.25;
+            kp.position.x = 0.27;
             kp.position.y = 0.00;
             kp.position.z = -0.1;
             kp.orientation = start.orientation;
-            if(!trajectory.addPose(kp, Trajectory::DEFAULT_MAX_VEL)) {
+            if(!trajectory.addPose(kp, 0.0)) {
                 ROS_ERROR("Unable to create trajectory for 2 poses");
                 return;
             }
@@ -181,12 +205,14 @@ class Manipulator {
             val.unit = "rad";
             for(int j=0; j<5;j++) {
                 std::stringstream ss;
-                ss << "arm_joint_" << (i+1);
+                ss << "arm_joint_" << (j+1);
                 val.joint_uri = ss.str().c_str();
-                val.value = point.positions[i];
+                val.value = point.positions[j];
                 msg.positions.push_back(val);
+                ROS_ERROR_STREAM("First Point: " << val.joint_uri << " has value " << val.value);
             }
             armPublisher.publish(msg);
+            // ATTENTION end
 
             // execute torque controlled trajectory
             ros::spinOnce();
@@ -205,4 +231,18 @@ class Manipulator {
 
 };
 
+}
+
+
+//----------------------------------- MAIN -----------------------------------------------
+int main(int argc, char** argv)
+{
+    // init node
+    ros::init(argc, argv, "cleaner");
+    ros::NodeHandle n;
+    ros::spinOnce();
+    youbot_proxy::Manipulator m(n);
+    ros::spinOnce();
+
+    m.testTrajectory();
 }
